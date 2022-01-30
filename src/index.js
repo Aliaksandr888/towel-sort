5f7f3639d8cef292d7b2d7c3b03c4700272cf8df
Aliaksandr <al_brain8@tut.by>
2022-01-30 22:23:40 +0300

    fixt:update file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr1 = [];
+  if(matrix.length ===0){
+    return arr1
+  }else{
+    for(let i=0;i<matrix.length;i++){
+      if(i%2===0){
+        for(let j=0;j<matrix[i].length;j++){
+          arr1.push(matrix[i][j]);
+        }
+        }else{
+          for(let k=matrix[i].length-1;k>=0;--k){
+            arr1.push(matrix[i][k]);
+          }
+      }
+     }
+      return arr1
+  }
+  
 }
